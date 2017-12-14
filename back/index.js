f74a963585d059a564c03a61d096fdb4ec573ea1
sitthisack <bandassak.sitthisack@gmail.com>
2017-12-14 14:51:16 +0100

    reglage code Crossorigin

--- back/index.js
+++ back/index.js
@@ -9,6 +9,12 @@ var _client = "";
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(function(req, res, next) {
+ res.header(`Access-Control-Allow-Origin`, `*`);
+ res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+ res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+ next();
+});
 
 mongo.connect(url, function(err, client) {
 	console.log("Connected successfully to server");
@@ -38,7 +44,13 @@ app.get('/getProduct', function(req, res) {
 	db.collection('Produit').find({}).toArray(function(err, docs){
 		res.status(200).send(docs);
 	});
-})
+});
+
+// app.post('/createUser', function(req, res) {
+// 	var body = req.body;
+// 	var db = _client.db('Shop');
+
+// });
 
 app.listen(3000, function() {
 	console.log('Server ON');
