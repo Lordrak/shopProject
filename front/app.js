f3dd5c1416777f9fc11a2b5714132b4226883e3c
GraceKitoko <gracekitoko92@gmail.com>
2017-12-20 11:54:01 +0100

    ui router fini

--- front/app.js
+++ front/app.js
@@ -1,1 +1,1 @@
-angular.module('Shop',[]);
+angular.module('Shop',['ui.router']);
---
+++ front/components/component_main.js
@@ -0,0 +1,36 @@
+angular.module('Shop').component('main',{
+
+	templateUrl: "views/view_main.html",
+
+	controller: "mainCtrl"
+
+});
+
+angular.module('Shop').config(function($stateProvider){
+
+	var createCompte = {
+
+		name: 'createCompte',
+		url: '/createCompte',
+		templateUrl: 'views/view_creationCompte.html'
+	};
+
+	var produit = {
+
+		name: 'produit',
+		url: '/produit',
+		templateUrl: 'views/view_produit.html'
+	};
+
+	var login = {
+
+		name: 'login',
+		url: '/login',
+		templateUrl: 'views/view_login.html'
+	}
+
+	$stateProvider.state(createCompte);
+	$stateProvider.state(produit);
+	$stateProvider.state(login);
+
+});
